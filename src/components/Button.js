f3ca995d38bc0a7a3111bc2c98bb140e6286c9dd
dennis law <denn15_law@live.ca>
2021-12-08 15:51:05 -0500

    create button component with styling props

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 import "components/Button.scss";
+import { action } from "@storybook/addon-actions/dist/preview";
+import classNames from "classnames";
 
 export default function Button(props) {
-   return <></>;
+  let buttonClass = classNames("button", {
+    "button--confirm": props.confirm,
+    "button--danger": props.danger,
+  });
+  return (
+    <button
+      className={buttonClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
 }
