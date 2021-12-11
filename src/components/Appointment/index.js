8a5cb98bfcf273d69bdfe7ab9ec21dfe6ea4b150
dennis law <denn15_law@live.ca>
2021-12-11 17:40:30 -0500

    add logic to use custom useVisualMode hook to render appointment components Empty, Show and Form

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,19 +3,29 @@ import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "hooks/useVisualMode";
+import Form from "./Form";
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? (
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
         <Show
-          student={props.interview["student"]}
-          interviewer={props.interview["interviewer"]}
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
         />
-      ) : (
-        <Empty />
       )}
+      {mode === CREATE && <Form interviewers={[]} onCancel={back} />}
     </article>
   );
 }
