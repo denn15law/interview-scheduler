a2fa331e204ad7effe9119449e56a9e7e51b16a6
dennis law <denn15_law@live.ca>
2021-12-11 17:59:17 -0500

    add getInterviewersForDay function and pass down interivewers array to Appointment to pass down to Form component

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,7 +4,11 @@ import "components/Application.scss";
 import axios from "axios";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-import { getAppointmentsForDay } from "helpers/selectors";
+import {
+  getAppointmentsForDay,
+  getInterview,
+  getInterviewersForDay,
+} from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
@@ -32,6 +36,7 @@ export default function Application(props) {
   }, []);
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const dailyInterviewers = getInterviewersForDay(state, state.day);
 
   const schedule = dailyAppointments.map((appointment) => {
     const interview = getInterview(state, appointment.interview);
@@ -41,6 +46,7 @@ export default function Application(props) {
         id={appointment.id}
         time={appointment.time}
         interview={interview}
+        interviewers={dailyInterviewers}
       />
     );
   });
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -25,7 +25,9 @@ export default function Appointment(props) {
           interviewer={props.interview.interviewer}
         />
       )}
-      {mode === CREATE && <Form interviewers={[]} onCancel={back} />}
+      {mode === CREATE && (
+        <Form interviewers={props.interviewers} onCancel={back} />
+      )}
     </article>
   );
 }
