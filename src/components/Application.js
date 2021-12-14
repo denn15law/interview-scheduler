063400479f079cbc4aac0878a3d2f646027e1616
dennis law <denn15_law@live.ca>
2021-12-14 00:57:05 -0500

    removed unused code

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "components/Application.scss";
-import axios from "axios";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 import {
