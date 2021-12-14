920740be343f4c09bee2bf825c22ae2c703525fb
dennis law <denn15_law@live.ca>
2021-12-13 20:58:16 -0500

    add ERROR_DELTE and ERROR_SAVE modes and adjust logic to replace history in transition function

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -7,15 +7,18 @@ import useVisualMode from "hooks/useVisualMode";
 import Form from "./Form";
 import Status from "./Status";
 import Confirm from "./Confirm";
+import Error from "./Error";
 
 export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
   const SAVING = "SAVING";
-  const DELETING = "DELETING";
+  const DELETE = "DELETING";
   const CONFIRM = "CONFIRM";
   const EDIT = "EDIT";
+  const ERROR_SAVE = "ERROR_SAVE";
+  const ERROR_DELETE = "ERROR_DELETE";
 
   const { id, time, interview, interviewers, bookInterview, cancelInterview } =
     props;
@@ -28,16 +31,24 @@ export default function Appointment(props) {
       student: name,
       interviewer,
     };
-    bookInterview(id, newInterview).then(() => {
-      transition(SHOW);
-    });
+    bookInterview(id, newInterview)
+      .then(() => {
+        transition(SHOW);
+      })
+      .catch(() => {
+        transition(ERROR_SAVE, true);
+      });
   };
 
   const deleteInterview = () => {
-    transition(DELETING);
-    cancelInterview(id).then(() => {
-      transition(EMPTY);
-    });
+    transition(DELETE, true);
+    cancelInterview(id)
+      .then(() => {
+        transition(EMPTY);
+      })
+      .catch(() => {
+        transition(ERROR_DELETE, true);
+      });
   };
 
   return (
@@ -49,19 +60,35 @@ export default function Appointment(props) {
           student={interview.student}
           interviewer={interview.interviewer}
           onDelete={() => transition(CONFIRM)}
+          onEdit={() => transition(EDIT)}
         />
       )}
       {mode === CREATE && (
         <Form interviewers={interviewers} onCancel={back} onSave={save} />
       )}
+      {mode === EDIT && (
+        <Form
+          student={interview.student}
+          interviewer={interview.interviewer.id}
+          interviewers={interviewers}
+          onCancel={back}
+          onSave={save}
+        />
+      )}
       {mode === SAVING && <Status message={SAVING} />}
-      {mode === DELETING && <Status message={DELETING} />}
+      {mode === DELETE && <Status message={DELETE} />}
       {mode === CONFIRM && (
         <Confirm
           message={"Are you sure you would like to delete?"}
           onCancel={back}
           onConfirm={deleteInterview}
         />
+      )}
+      {mode === ERROR_SAVE && (
+        <Error message={"Could not save appointment"} onClose={back} />
+      )}
+      {mode === ERROR_DELETE && (
+        <Error message={"Could not delete appointment"} onClose={back} />
       )}
     </article>
   );
