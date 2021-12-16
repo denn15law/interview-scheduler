e1d05ae3ea3b5569bcfbc452060798a541b823fa
dennis law <denn15_law@live.ca>
2021-12-15 21:05:16 -0500

    add data-testid class for cypress and refractor test to use classes to check functionality

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -6,8 +6,8 @@ describe("Navigation", () => {
   it("should navigate to tueday", () => {
     cy.visit("/");
 
-    cy.contains("li", "Tuesday")
+    cy.contains("[data-testid=day]", "Tuesday")
       .click()
-      .should("have.css", "background-color", "rgb(242, 242, 242)");
+      .should("have.class", "day-list__item--selected");
   });
 });
